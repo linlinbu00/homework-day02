332629af352bd9553a698eed17e1b8a845449e84
linlin <2570965697@qq.com>
2022-06-19 19:13:38 +0800

    完成了第二天作业

---
+++ 01帅哥美女走一走/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(item, index) in arr" :key="index">{{ item }}</li>
+    </ul>
+    <button @click="run">走一走</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      arr: ['美女', '帅哥', '程序员'],
+    };
+  },
+  methods: {
+    run() {
+      // 先把第一项推到数组最后
+      this.arr.push(this.arr[0]);
+      // 再把第一项删了
+      this.arr.shift(this.arr[0]);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ 02加加减减/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(item, index) in arr" :key="index">
+        {{ item }}
+        <!-- 将index传入 -->
+        <button @click="del(index)">删除</button>
+      </li>
+    </ul>
+    <button @click="add">生成</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      arr: [1, 5, 3],
+    };
+  },
+  methods: {
+    add() {
+      this.arr.push(parseInt(Math.random() * 10 + 1));
+    },
+    // 接收参数
+    del(index) {
+    // 从当前索引移除一项---删除当前元素
+      this.arr.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style></style>
